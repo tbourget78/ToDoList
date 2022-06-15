7452736bbbfdd3708634af6a676ab5841ba8d887
Thomas Bourget <t.bourget78@live.fr>
2022-06-15 09:32:01 +0200

    mise en place du responsive

--- script.js
+++ script.js
@@ -3,22 +3,44 @@
 ol.innerHTML = localStorage.getItem("list");
 
 const spanDels = document.querySelectorAll(".delete");
-
 for (let span of spanDels) {
-  span.onclick = () => del(span.parentElement);
+  span.onclick = () => del(span.parentElement.parentElement);
+}
+
+const spanUrg = document.querySelectorAll(".urgent");
+for (let span of spanUrg) {
+  span.onclick = () => urgent(span);
 }
 
 noTache.style.display = ol.innerHTML == "" ? "block" : "none";
 
 form.onsubmit = () => {
   const li = document.createElement("li");
-  const spanDel = document.createElement("span");
-  spanDel.classList.add("delete");
-  spanDel.textContent = "(X)";
-  spanDel.onclick = () => del(li);
-  li.innerHTML = champ.value;
 
-  li.appendChild(spanDel);
+  const spanDel = document.createElement("span");
+  spanDel.classList.add("delete", "material-icons", "md-24");
+  spanDel.textContent = "clear";
+
+  const spanUrg = document.createElement("span");
+  spanUrg.classList.add("urgent", "material-icons", "md-24");
+  spanUrg.textContent = "stars";
+
+  const texte = document.createElement("span");
+  texte.classList.add("texte");
+  texte.textContent = champ.value;
+
+  spanDel.onclick = () => del(li);
+  spanUrg.onclick = () => urgent(spanUrg);
+
+  const spanOpt = document.createElement("span");
+  spanOpt.classList.add("spanOpt");
+
+  spanOpt.appendChild(spanUrg);
+  spanOpt.appendChild(spanDel);
+
+  li.appendChild(texte);
+  li.appendChild(spanOpt);
+
   ol.appendChild(li);
 
   champ.value = "";
@@ -35,3 +57,13 @@ function del(element) {
 
   localStorage.setItem("list", ol.innerHTML);
 }
+
+function urgent(element) {
+  element.classList.toggle("gold");
+  localStorage.setItem("list", ol.innerHTML);
+}
+
+/* resizing */ /* resizing */ /* resizing */ /* resizing */ /* resizing */ /* resizing */ /* resizing */
+
+const metas = document.getElementsByTagName("meta");
+metas[1].content = `width=device-width, height= ${window.innerHeight},initial-scale=1.0`;
